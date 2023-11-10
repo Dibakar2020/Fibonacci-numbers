3be131f8398446737a5636517c72ce72eb3040c2
Dibakar Pal <150482439+Dibakar2020@users.noreply.github.com>
2023-11-11 02:09:23 +0530

    Add files via upload

---
+++ module4.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence)<n:
+        next_fib_number=fib_sequence[-1]+fib_sequence[-2]
+        fib_sequence.append(next_fib_number)
+    return fib_sequence
+#for printing 1st ten fibonacci number
+first_ten_fib_number=fibonacci(10)
+print("frist ten Fibonacci number",first_ten_fib_number)
